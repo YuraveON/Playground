5126ab6f719bd393aa8ab16ee0eea32e23505d86
Yulio Ramadhan <yulio4ramadhan@gmail.com>
2020-11-06 00:24:32 +0700

    add multithreading

--- WebScrapping/v2.py
+++ WebScrapping/v2.py
@@ -1,20 +1,22 @@
 import csv
 import errno
 from socket import error as SocketError
-
 import requests
 from bs4 import BeautifulSoup
+import concurrent.futures, time
 
-try:
 
-    for page in range(907):
+MAX_THREADS = 30
+
+def scrap(page):
+    try:
         url = f"https://indopos.co.id/page/{page}/?s=corona"
         header = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
         }
         results = requests.get(url, headers=header)
         status = results.status_code
-        print(f'Scrapping halaman {page}')
+        print(f'Scrapping halaman {page}\n')
 
         if status == 200:
             soup = BeautifulSoup(results.text, "html.parser")
@@ -28,21 +30,29 @@ try:
                     kategori = be.find('div', 'term-badges floated').text.strip()
                 except AttributeError:
                     kategori = ''
-                beritas.append([judul, tanggal, kategori])
+                beritas.append([judul, tanggal, kategori, page])
 
             with open(r'results/berita_tanpa_batas.csv', 'a', newline='', encoding='utf-8') as file:
                 for d in beritas:
                     writer = csv.writer(file)
                     writer.writerow(d)
 
+            time.sleep(0.25)
+
         else:
             print('Halaman habis')
-            break
+            exit()
+
+    except SocketError as e:
+        print("Servernya ngadat brow")
+        if e.errno != errno.ECONNRESET:
+            raise  # Not error we are looking for
+        pass  # Handle error here.
 
 
+if __name__ == '__main__':
+    batas = range(200,205)
+    threads = min(MAX_THREADS, len(batas))
 
-except SocketError as e:
-    print("Servernya ngadat brow")
-    if e.errno != errno.ECONNRESET:
-        raise  # Not error we are looking for
-    pass  # Handle error here.
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        executor.map(scrap, batas)
